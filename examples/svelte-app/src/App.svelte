7ab005f8ee9daa54cebaf4509f9678835ec34382
ntargon <argon19951128@gmail.com>
2022-11-06 19:46:19 +0900

    exampleを変更
    yarn create tauri-appで自分で作った

--- examples/svelte-app/src/App.svelte
+++ examples/svelte-app/src/App.svelte
@@ -1,18 +1,38 @@
 <script lang="ts">
+  import Greet from './lib/Greet.svelte'
 	import { execute } from 'tauri-plugin-comm-api'
+	import { invoke } from '@tauri-apps/api'
 
 	let response = ''
 
 	function updateResponse(returnValue) {
+    console.log(returnValue);
 		response += `[${new Date().toLocaleTimeString()}]` + (typeof returnValue === 'string' ? returnValue : JSON.stringify(returnValue)) + '<br>'
 	}
 
-	function _execute() {
+	async function _execute() {
 		execute().then(updateResponse).catch(updateResponse)
+    let a = await invoke('plugin:comm|execute')
+		console.log(a);
+
 	}
 </script>
+
+<main class="container">
 
 <div>
 	<button on:click="{_execute}">Execute</button>
 	<div>{@html response}</div>
 </div>
+
+</main>
+
+<style>
+  .logo.vite:hover {
+    filter: drop-shadow(0 0 2em #747bff);
+  }
+
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00);
+  }
+</style>
--- examples/svelte-app/src/main.ts
+++ examples/svelte-app/src/main.ts
@@ -1,8 +1,8 @@
-import App from './App.svelte'
+import "./style.css";
+import App from "./App.svelte";
 
 const app = new App({
-  target: document.body,
-  props: {}
-})
+  target: document.getElementById("app"),
+});
 
-export default app
+export default app;
